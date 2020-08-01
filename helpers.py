dbaabfe3056f07895c1e705465c66a12bb7294c6
merchyaqua <merchyaqua@gmail.com>
2020-08-01 05:23:37 +0000

    update

--- helpers.py
+++ helpers.py
@@ -113,6 +113,28 @@ def ordinal(value):
 
     return ordval
 
+# Converts a list of SQL Alchemy RowProxy objects into a list of dictionary objects with the column name as the key (https://github.com/cs50/python-cs50/blob/develop/src/cs50/sql.py#L328)
+# Used for SQL SELECT .fetchall() results
+def convertSQLToDict(listOfRowProxy):
+    # Coerce types
+    rows = [dict(row) for row in listOfRowProxy]
+    for row in rows:
+        for column in row:
+
+            # Coerce decimal.Decimal objects to float objects
+            # https://groups.google.com/d/msg/sqlalchemy/0qXMYJvq8SA/oqtvMD9Uw-kJ
+            if type(row[column]) is decimal.Decimal:
+                row[column] = float(row[column])
+
+            # Coerce memoryview objects (as from PostgreSQL's bytea columns) to bytes
+            elif type(row[column]) is memoryview:
+                row[column] = bytes(row[column])
+
+    return rows
+
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
